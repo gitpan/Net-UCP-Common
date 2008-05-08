be18b76618faae65fd442df815cbf5b4dd4888aa
Marco Romano <nemux@cpan.org>
2008-05-08 15:06:07 +0000

    Import of NEMUX/Net-UCP-Common-0.05 from CPAN.
    gitpan-cpan-distribution: Net-UCP-Common
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         NEMUX/Net-UCP-Common-0.05.tar.gz
    gitpan-cpan-author:       NEMUX
    gitpan-cpan-maturity:     released

--- lib/Net/UCP/Common.pm
+++ lib/Net/UCP/Common.pm
@@ -12,7 +12,7 @@ our %EXPORT_TAGS = ( 'all' => [ qw(STX ETX UCP_DELIMITER DEF_SMSC_PORT ACK NACK 
 our @EXPORT_OK   = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT      = qw();
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use constant STX           => chr(2);
 use constant ETX           => chr(3);
@@ -161,6 +161,7 @@ sub _init {
 		     );
     
     $self->{EC} = %ec_string;
+    $self;
 }
 
 
